e3605e99fdb54bfef283ada5b8e595ee2b56d93a
foysal777 <foysal.cse11@gmail.com>
2024-10-06 23:27:31 +0600

    fetch

--- profile.js
+++ profile.js
@@ -7,3 +7,30 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+
+fetch('https://water-backend-api.vercel.app/account/api/user-role/', {
+    method: 'GET',
+    headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+    }
+})
+.then(response => {
+    if (response.ok) {
+        return response.json();  
+    } else {
+        throw new Error('Network response was not ok');
+    }
+})
+.then(data => {
+    console.log(data);
+    const profileCard = document.getElementById('profile-card');
+    profileCard.innerHTML = `
+        <img src="images/profile.png" alt="User Photo">
+        <h4>${data.first_name}</h4>
+        <p><strong>Username:</strong> ${data.username}</p>
+        <p><strong>Email:</strong> ${data.email}</p>
+    `;
+})
+.catch(error => console.error('Error fetching user data:', error));
